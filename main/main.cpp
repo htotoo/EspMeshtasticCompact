3fea5f0b41ae51d18f33aff9119adbb92f44276b
HTotoo <ttotoo@gmail.com>
2025-07-04 14:09:33 +0200

    better output

--- main/main.cpp
+++ main/main.cpp
@@ -29,13 +29,23 @@ extern "C" {
 void app_main();
 }
 
+void PrintHeaderInfo(MC_Header& header) {
+    ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
+    ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB", header.rssi, header.snr);
+    auto sender = meshtasticCompact.nodeinfo_db.get(header.srcnode);
+    if (sender) {
+        ESP_LOGI(TAG, "Sender Node: Short Name: %s, Long Name: %s", sender->short_name, sender->long_name);
+    } else {
+        ESP_LOGW(TAG, "Sender Node Info not found in database for Node ID: 0x%08" PRIx32, header.srcnode);
+    }
+}
+
 void app_main(void) {
     meshtasticCompact.RadioInit();
     meshtasticCompact.setOnMessage([](MC_Header header, MC_TextMessage message) {
         ESP_LOGI(TAG, "Received message on channel %d: %s", message.chan, message.text.c_str());
         ESP_LOGI(TAG, "Msg type %d", message.type);
-        ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
-        ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB", header.rssi, header.snr);
+        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnPositionMessage([](MC_Header header, MC_Position position) {
         ESP_LOGI(TAG, "Received position update:");
@@ -43,8 +53,7 @@ void app_main(void) {
         ESP_LOGI(TAG, "Ground Speed: %lu", position.ground_speed);
         ESP_LOGI(TAG, "Satellites in view: %lu", position.sats_in_view);
         ESP_LOGI(TAG, "Location Source: %u", position.location_source);
-        ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
-        ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB", header.rssi, header.snr);
+        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnNodeInfoMessage([](MC_Header header, MC_NodeInfo nodeinfo) {
         ESP_LOGI(TAG, "Received node info:");
@@ -57,8 +66,7 @@ void app_main(void) {
                  nodeinfo.macaddr[3], nodeinfo.macaddr[4], nodeinfo.macaddr[5]);
         // ESP_LOGI(TAG, "Public Key: %s", nodeinfo.public_key);
         ESP_LOGI(TAG, "Role: %u", nodeinfo.role);
-        ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
-        ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB", header.rssi, header.snr);
+        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnWaypointMessage([](MC_Header header, MC_Waypoint waypoint) {
         ESP_LOGI(TAG, "Received waypoint:");
@@ -67,8 +75,7 @@ void app_main(void) {
         ESP_LOGI(TAG, "Description: %s", waypoint.description);
         ESP_LOGI(TAG, "Latitude: %ld, Longitude: %ld", waypoint.latitude_i, waypoint.longitude_i);
         ESP_LOGI(TAG, "Icon: %lu, Expire: %lu", waypoint.icon, waypoint.expire);
-        ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
-        ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB", header.rssi, header.snr);
+        PrintHeaderInfo(header);
     });
     while (1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
