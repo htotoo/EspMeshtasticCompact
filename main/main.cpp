8c9611b3528f0c1a602c3b64a2ad43df2a669f1e
HTotoo <ttotoo@gmail.com>
2025-07-08 15:02:52 +0200

    visual fix

--- main/main.cpp
+++ main/main.cpp
@@ -37,7 +37,7 @@ void app_main();
 }
 
 void PrintHeaderInfo(MC_Header& header) {
-    ESP_LOGI(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
+    ESP_LOGW(TAG, "Source Node ID: 0x%08" PRIx32 ", Destination Node ID: 0x%08" PRIx32 ", Hop Limit: %d", header.srcnode, header.dstnode, header.hop_limit);
     ESP_LOGI(TAG, "Last signal data - RSSI: %.2f dBm, SNR: %.2f dB. WantAck: %d", header.rssi, header.snr, header.want_ack ? 1 : 0);
     auto sender = meshtasticCompact.nodeinfo_db.get(header.srcnode);
     if (sender) {
@@ -48,8 +48,9 @@ void PrintHeaderInfo(MC_Header& header) {
 }
 
 void app_main(void) {
-    MeshtasticCompactHelpers::PositionBuilder(meshtasticCompact.my_position, 47.123456f, 8.123456f, 500, 0, 5);
+    MeshtasticCompactHelpers::PositionBuilder(meshtasticCompact.my_position, 47.123456f, 18.123456f, 500, 0, 5);
     meshtasticCompact.RadioInit();
+
     meshtasticCompact.setOnMessage([](MC_Header& header, MC_TextMessage& message) {
         ESP_LOGI(TAG, "Received message on channel %d: %s", message.chan, message.text.c_str());
         ESP_LOGI(TAG, "Msg type %d", message.type);
@@ -57,17 +58,18 @@ void app_main(void) {
     });
     meshtasticCompact.setOnPositionMessage([](MC_Header& header, MC_Position& position, bool needReply) {
         ESP_LOGI(TAG, "Received position update:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Latitude: %ld, Longitude: %ld, Altitude: %ld", position.latitude_i, position.longitude_i, position.altitude);
         ESP_LOGI(TAG, "Ground Speed: %lu", position.ground_speed);
         ESP_LOGI(TAG, "Satellites in view: %lu", position.sats_in_view);
         ESP_LOGI(TAG, "Location Source: %u", position.location_source);
-        PrintHeaderInfo(header);
         if (needReply && header.dstnode == meshtasticCompact.getMyNodeInfo()->node_id) {
             meshtasticCompact.SendMyPosition();  // Reply to the sender
         }
     });
     meshtasticCompact.setOnNodeInfoMessage([](MC_Header& header, MC_NodeInfo& nodeinfo, bool needReply) {
         ESP_LOGI(TAG, "Received node info:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Node ID: %s", nodeinfo.id);
         ESP_LOGI(TAG, "Short Name: %s", nodeinfo.short_name);
         ESP_LOGI(TAG, "Long Name: %s", nodeinfo.long_name);
@@ -77,7 +79,6 @@ void app_main(void) {
                  nodeinfo.macaddr[3], nodeinfo.macaddr[4], nodeinfo.macaddr[5]);
         // ESP_LOGI(TAG, "Public Key: %s", nodeinfo.public_key);
         ESP_LOGI(TAG, "Role: %u", nodeinfo.role);
-        PrintHeaderInfo(header);
 
         if (needReply && header.dstnode == meshtasticCompact.getMyNodeInfo()->node_id) {
             meshtasticCompact.SendMyNodeInfo(header.srcnode);  // Reply to the sender
@@ -85,31 +86,32 @@ void app_main(void) {
     });
     meshtasticCompact.setOnWaypointMessage([](MC_Header& header, MC_Waypoint& waypoint) {
         ESP_LOGI(TAG, "Received waypoint:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Waypoint ID: %lu", waypoint.id);
         ESP_LOGI(TAG, "Name: %s", waypoint.name);
         ESP_LOGI(TAG, "Description: %s", waypoint.description);
         ESP_LOGI(TAG, "Latitude: %ld, Longitude: %ld", waypoint.latitude_i, waypoint.longitude_i);
         ESP_LOGI(TAG, "Icon: %lu, Expire: %lu", waypoint.icon, waypoint.expire);
-        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnTelemetryDevice([](MC_Header& header, MC_Telemetry_Device& telemetry) {
         ESP_LOGI(TAG, "Received telemetry device data:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Battery Voltage: %.2f V", telemetry.voltage);
         ESP_LOGI(TAG, "Battery Percent: %lu", telemetry.battery_level);
         ESP_LOGI(TAG, "channel_utilization: %.2f °C", telemetry.channel_utilization);
         ESP_LOGI(TAG, "Uptime: %lu seconds", telemetry.uptime_seconds);
-        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnTelemetryEnvironment([](MC_Header& header, MC_Telemetry_Environment& telemetry) {
         ESP_LOGI(TAG, "Received telemetry environment data:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Temperature: %.2f °C", telemetry.temperature);
         ESP_LOGI(TAG, "Humidity: %.2f %%", telemetry.humidity);
         ESP_LOGI(TAG, "Pressure: %.2f hPa", telemetry.pressure);
         ESP_LOGI(TAG, "Lux: %.2f", telemetry.lux);
-        PrintHeaderInfo(header);
     });
     meshtasticCompact.setOnTraceroute([](MC_Header& header, MC_RouteDiscovery& route, bool for_me, bool is_reply, bool need_reply) {
         ESP_LOGI(TAG, "Received traceroute packet:");
+        PrintHeaderInfo(header);
         ESP_LOGI(TAG, "Hop Limit: %d, Hop Start: %d", header.hop_limit, header.hop_start);
         ESP_LOGI(TAG, "Route Count: %zu", route.route_count);
         for (int i = 0; i < route.route_count; ++i) {
@@ -128,7 +130,6 @@ void app_main(void) {
             ESP_LOGI(TAG, "SNR Back Hop %d: %d dB", i + 1, route.snr_back[i]);
         }
         ESP_LOGI(TAG, "For Me: %s, Is Reply: %s, Need Reply: %s", for_me ? "true" : "false", is_reply ? "true" : "false", need_reply ? "true" : "false");
-        PrintHeaderInfo(header);
     });
 
     meshtasticCompact.SendMyNodeInfo(0xffffffff, true);
