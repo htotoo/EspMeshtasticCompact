2762b83bf9c801b1f2d621d7cfb2105eab08365c
HTotoo <ttotoo@gmail.com>
2025-09-09 13:08:01 +0200

    fixes

--- include/MeshtasticCompact.hpp
+++ include/MeshtasticCompact.hpp
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <string.h>
 #include "pb.h"
-#include <RadioLib.h>
+#include "RadioLib.h"
 #include "EspHal.h"
 #include "mbedtls/aes.h"
 #include <string>
@@ -487,7 +487,7 @@ class MeshtasticCompact {
  */
 class MeshtasticCompactHelpers {
    public:
-    static void NodeInfoBuilder(MC_NodeInfo& nodeinfo, uint32_t node_id, std::string& short_name, std::string& long_name, uint8_t hw_model);
+    static void NodeInfoBuilder(MC_NodeInfo* nodeinfo, uint32_t node_id, std::string& short_name, std::string& long_name, uint8_t hw_model);
     static void PositionBuilder(MC_Position& position, float latitude, float longitude, int32_t altitude = 0, uint32_t speed = 0, uint32_t sats_in_view = 0);
     static void TelemetryDeviceBuilder(MC_Telemetry_Device& telemetry, uint32_t uptime_seconds = 0, float voltage = 0.0f, float battery_level = -1.0f, float channel_utilization = -1.0f);
     static void TelemetryEnvironmentBuilder(MC_Telemetry_Environment& telemetry, float temperature = -10000.0f, float humidity = -1.0f, float pressure = -1.0f, float lux = -1.0f);
--- src/MeshtasticCompact.cpp
+++ src/MeshtasticCompact.cpp
@@ -1098,8 +1098,8 @@ inline uint8_t MeshtasticCompact::getLastByteOfNodeNum(uint32_t num) {
 
 #pragma region MeshtasticCompactHelpers
 
-void MeshtasticCompactHelpers::NodeInfoBuilder(MC_NodeInfo& nodeinfo, uint32_t node_id, std::string& short_name, std::string& long_name, uint8_t hw_model) {
-    nodeinfo.node_id = node_id;
+void MeshtasticCompactHelpers::NodeInfoBuilder(MC_NodeInfo* nodeinfo, uint32_t node_id, std::string& short_name, std::string& long_name, uint8_t hw_model) {
+    nodeinfo->node_id = node_id;
     if (long_name.empty()) {
         char hex_part[7];
         snprintf(hex_part, sizeof(hex_part), "%06" PRIx32, (node_id & 0xFFFFFF));
@@ -1114,20 +1114,20 @@ void MeshtasticCompactHelpers::NodeInfoBuilder(MC_NodeInfo& nodeinfo, uint32_t n
         short_name = std::string(hex_part);
     }
 
-    strncpy(nodeinfo.short_name, short_name.c_str(), sizeof(nodeinfo.short_name) - 1);
-    nodeinfo.short_name[sizeof(nodeinfo.short_name) - 1] = '\0';
-    strncpy(nodeinfo.long_name, long_name.c_str(), sizeof(nodeinfo.long_name) - 1);
-    nodeinfo.long_name[sizeof(nodeinfo.long_name) - 1] = '\0';
-    nodeinfo.hw_model = (uint8_t)meshtastic_HardwareModel_DIY_V1;
-    snprintf(nodeinfo.id, sizeof(nodeinfo.id), "!%08" PRIx32, node_id);
-    nodeinfo.id[sizeof(nodeinfo.id) - 1] = '0';
-    nodeinfo.role = 0;
+    strncpy(nodeinfo->short_name, short_name.c_str(), sizeof(nodeinfo->short_name) - 1);
+    nodeinfo->short_name[sizeof(nodeinfo->short_name) - 1] = '\0';
+    strncpy(nodeinfo->long_name, long_name.c_str(), sizeof(nodeinfo->long_name) - 1);
+    nodeinfo->long_name[sizeof(nodeinfo->long_name) - 1] = '\0';
+    nodeinfo->hw_model = (uint8_t)meshtastic_HardwareModel_DIY_V1;
+    snprintf(nodeinfo->id, sizeof(nodeinfo->id), "!%08" PRIx32, node_id);
+    nodeinfo->id[sizeof(nodeinfo->id) - 1] = '0';
+    nodeinfo->role = 0;
     for (int i = 0; i < 6; ++i) {
-        nodeinfo.macaddr[i] = (node_id >> (8 * (5 - i))) & 0xFF;
+        nodeinfo->macaddr[i] = (node_id >> (8 * (5 - i))) & 0xFF;
     }
-    memset(nodeinfo.public_key, 0, sizeof(nodeinfo.public_key));
-    nodeinfo.public_key_size = 0;  // Set to 0 if no public key is available
-    nodeinfo.hw_model = hw_model;  // Set hardware model
+    memset(nodeinfo->public_key, 0, sizeof(nodeinfo->public_key));
+    nodeinfo->public_key_size = 0;  // Set to 0 if no public key is available
+    nodeinfo->hw_model = hw_model;  // Set hardware model
 }
 
 void MeshtasticCompactHelpers::PositionBuilder(MC_Position& position, float latitude, float longitude, int32_t altitude, uint32_t speed, uint32_t sats_in_view) {
